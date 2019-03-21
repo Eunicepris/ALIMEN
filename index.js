d1bd6fd3c0a9dbaf356b61c39f87c4141a4d0bd3
Eunicepris <eunice.priscille@gmail.com>
2019-03-21 04:11:47 +0000

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
-
-app.set('views',__dirname+'VIEW')
+const path = require ('path')
+app.set('views', path.join(__dirname, 'VIEW'));
 app.set('view engine', 'ejs')
-app.use(express.static('public', options))
-
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.render("index");
+  });
+  app.get('/form', function(req, res) {
+    res.render("form");
+  });
+  app.get('/joni', function(req, res) {
+    res.render("joni");
+  });
+  app.get('/mail', function(req, res) {
+    res.render("mail");
+  });
+  app.get('/password', function(req, res) {
+    res.render("password");
+  });
+ 
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
 })
