09b804dd4313528f3da997dd7b4504e4b9d5447b
Eunicepris <eunice.priscille@gmail.com>
2019-03-21 01:50:19 +0000

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('views',__dirname+'VIEW')
+app.set('view engine', 'ejs')
+app.use(express.static('public', options))
+
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
 })
