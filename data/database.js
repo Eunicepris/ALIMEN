9d8eda39dfaa5795e9168f26d62f2ac877d45a1c
Eunicepris <eunice.priscille@gmail.com>
2019-03-26 20:21:13 +0000

    second commit

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ var connection=mysql.createConnection({
   host:'localhost',
   user:'root',
   password:'',
-  database:'profil'
+  database:'aliment'
 });
 connection.connect(function(error){
   if(!!error){
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const path = require ('path')
 const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
+const { check } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const expressValidator = require ('express-validator')
 var connection = require("../ALIMEN-master/data/database")
 
 
@@ -14,43 +16,91 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'VIEW'));
 app.set('view engine', 'ejs')
 
-app.use((err,req,res,next ) => {
-    
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
- })
-
   
-  app.get('/', function(req, res) {
-    res.render("index");
+  app.get('/alimant', function(req, res) {
+    res.render("line");
   });
+
   app.get('/form', function(req, res) {
     res.render("form");
   });
   
-  app.get('/joni', function(req, res) {
-    res.render("joni");
-  });
-  app.get('/mail', function(req, res) {
-    res.render("mail");
-  });
+ 
   app.post('/', function(req, res, next) {
     res.render("index");
     console.log(req.body)
+
+    let values = [req.body.name, req.body.email, req.body.password, req.body.lastname, req.body.phone,req.body.passwordconfirm ]
+     
+    
+    traitementForm(function(req, res, next)
+    {
+      check('email').isEmail(),
+    check('password').isLength({ min: 5 }),
+
+    check('name').isLength({ min:2 }),
+    check('lastname').isLength({ min:2 }),
+
+    check('phone').isLength({ min:8,max:8 }),
+
+    check("password", "invalid password")
+     .isLength({ min: 5 })
+     .custom((value,{req, loc, path}) => {
+        if (value !== req.body.passwordconfirm) {
+            // trow error if passwords do not match
+            throw new Error("Passwords don't match");
+        } else {
+            return value;
+        }
+    })
+    })
+
   });
-  app.get('/password', function(req, res) {
+
+  // route des utilisateurs //
+  app.get('/alimant/connexion/password', function(req, res) {
     res.render("password");
   });
+
+  app.get('/alimant/connexion/mail', function(req, res) {
+    res.render("mail");
+  });
+
+  app.get('/alimant/inscription', function(req, res) {
+    res.render("inscription");
+  });
+
+  app.post('/alimant/inscription', function(req, res){
+    connection.query('INSERT INTO utilisateurs(name, email, password, lastname, phone, passwordconfirm) VALUES(?, ?, ?, ?, ?, ?)', 
+    [req.body.name, req.body.email, req.body.password, req.body.lastname, req.body.phone, req.body.passwordconfirm], function(err, result){
+         if(err){
+           console.log(err.message)
+         }
+         else
+         {
+           res.redirect('/alimant/connexion/mail')
+         }
+    })
+  })
+
  
   app.get('/ahiline', function(req, res) {
     res.render("ahiline");
   });
-  app.get('/line', function(req, res) {
-    res.render("line");
-  });
+
 app.listen(3000, function(){
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
+})
+
+
+
+
+//route des recettes//
+
+app.use((err,req,res,next ) => {
+  console.log.err
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+res.status(err.status || 500);
 })
