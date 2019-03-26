3057d04ad908f6aa3ff22e88a00ea5b9d6c2ac74
Eunicepris <eunice.priscille@gmail.com>
2019-03-26 01:53:10 +0000

    second message

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require('express')
 const app = express()
 const path = require ('path')
-const bodyPaser = require('body-paser')
+const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 var connection = require("../ALIMEN-master/data/database")
 
 
 app.use(expressValidator())
-app.use(bodyPaser.json())
-app.use(bodyPaser.urlencoded  ({extended :true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded  ({extended :true}))
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', path.join(__dirname, 'VIEW'));
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
+
+app.use((err,req,res,next ) => {
+    
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+ })
+
+  
+  app.get('/', function(req, res) {
     res.render("index");
   });
   app.get('/form', function(req, res) {
     res.render("form");
   });
-
-  app.use((err,req,res,next ) => {
-    
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
-   res.status(err.status || 500);
-   res.render('error');
-  })
-
   
   app.get('/joni', function(req, res) {
     res.render("joni");
@@ -36,8 +37,9 @@ app.get('/', function(req, res) {
   app.get('/mail', function(req, res) {
     res.render("mail");
   });
-  app.post('/mail', function(req, res) {
-    res.render("mail");
+  app.post('/', function(req, res, next) {
+    res.render("index");
+    console.log(req.body)
   });
   app.get('/password', function(req, res) {
     res.render("password");
